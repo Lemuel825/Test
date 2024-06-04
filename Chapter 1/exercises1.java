0d9c192ef825e42c3e5b74f1a1e0016d8e9c7098
Lemuel825 <140239366+Lemuel825@users.noreply.github.com>
2024-06-04 09:53:48 +0000

    done input

---
+++ Chapter 1/exercises1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class exercises1 {
+public static int Fnum,Snum;
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input First Number: ");
+        Fnum = in.nextInt();
+        System.out.print("Input Second Number: ");
+        Snum = in.nextInt();
+        System.out.print(Fnum + " " + Snum);
+
+        
+    }
+}
