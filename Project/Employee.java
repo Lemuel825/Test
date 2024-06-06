42ee0296c474578eb8d87427271463d66d6e2b34
Lemuel825 <lem25.tresvalles@gmail.com>
2024-06-07 01:43:59 +0800

    unfinished

---
+++ Project/Employee.java
@@ -0,0 +1,35 @@
+package Project;
+
+public class Employee {
+    int EmployeeID = 1001;
+    String Name;
+
+    public Employee(int EmployeeID, String Name) {
+        this.EmployeeID = EmployeeID;
+        this.Name = Name;
+    }
+
+    public int getEmployeeID() {
+        return EmployeeID;
+    }
+
+    public void setEmployeeID(int EmployeeID) {
+        this.EmployeeID = EmployeeID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void AddEmployee()
+    {
+        
+    }
+
+
+
+}
---
+++ Project/Main.java
@@ -0,0 +1,16 @@
+package Project;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee;
+
+        ArrayList<Employee> listOfEmployees = new ArrayList<Employee>();
+        Accounts a1 = new Accounts(1001,"admin1","pass1");
+
+        //listOfEmployees.add(employee.)//
+    }
+
+
+}
