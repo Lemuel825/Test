832bc29494a491409db6d4c8e3d35b787a866281
Lemuel825 <140239366+Lemuel825@users.noreply.github.com>
2024-06-08 03:35:12 +0000

    none

--- Project/Employee.java
+++ Project/Employee.java
@@ -1,13 +1,15 @@
 package Project;
 
 public class Employee {
-    int EmployeeID = 1001;
-    String Name;
+   private int EmployeeID;
+   private String Name;
+   private String Password;
 
-    public Employee(int EmployeeID, String Name) {
-        this.EmployeeID = EmployeeID;
-        this.Name = Name;
-    }
+    public Employee(int employeeID, String name, String password) {
+    EmployeeID = employeeID;
+    Name = name;
+    Password = password;
+}
 
     public int getEmployeeID() {
         return EmployeeID;
@@ -25,9 +27,19 @@ public class Employee {
         this.Name = Name;
     }
 
-    public void AddEmployee()
+    public String getPassword() {
+        return Password;
+    }
+    
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public void AddEmployee(Employee employee)
     {
-        
+        this.EmployeeID = employee.EmployeeID;
+        this.Name = employee.Name;
+        this.Password = employee.Password;
     }
 
 
--- Project/Main.java
+++ Project/Main.java
@@ -1,15 +1,82 @@
 package Project;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    static Employee employee;
+    public static ArrayList<Employee> listOfEmployees = new ArrayList<Employee>();
+
     public static void main(String[] args) {
-        Employee employee;
+        Scanner in = new Scanner(System.in);
+        boolean online = true;
+        Employee a1 = new Employee(1001,"admin1","pass1"); //default ACCOUNT
+        listOfEmployees.add(a1);
 
-        ArrayList<Employee> listOfEmployees = new ArrayList<Employee>();
-        Accounts a1 = new Accounts(1001,"admin1","pass1");
+        while (online) {
+            System.out.println("============ Coffee Shop ============");
+            System.out.print("\n[1] Login \n[2] Register \n[3] Exit\n\n Selected Choice: ");
+            int choice = in.nextInt();
+    
+            switch (choice) {
+                case 1:
+                DisplayLogin();
+                    break;
+                case 2:
+                register_Employee();
+                    break;
+            
+                case 3:
+                    
+                    break;
+                default:
+                    break;
+            }
+        }
+      
 
-        //listOfEmployees.add(employee.)//
+
+
+
+    }
+    public static void DisplayLogin(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("============ Register ============");
+        System.out.print("Input User ID: ");
+        int userID = in.nextInt();
+        System.out.print("Input Password: ");
+        String password = in.next();
+
+        for(Employee employee: listOfEmployees){
+            if(employee.getEmployeeID() == userID){
+                System.out.println("asdasd1");
+            }
+            else{
+                System.out.println("invalid1");
+
+            }
+        }
+
+    }
+
+
+    public static void register_Employee(){
+
+        Scanner in = new Scanner(System.in);
+        int index = listOfEmployees.size()-1;
+
+        int ID = listOfEmployees.get(index).getEmployeeID()+1;
+        System.out.println("============ Register ============");
+        System.out.println(ID);
+        System.out.print("Input Username: ");
+        String username = in.next();
+        System.out.print("Input Password: ");
+        String password = in.next();
+
+        Employee newEmployee = new Employee(ID,username,password);
+
+        listOfEmployees.add(newEmployee);
     }
 
 
